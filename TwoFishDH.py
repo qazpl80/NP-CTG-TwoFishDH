5d0a0ecb4a686ba93f182fc45a0bb681df904511
Zhe Yu Chua <s10196605@connect.np.edu.sg>
2020-01-28 17:35:20 +0800

    ehwehewheqhdu

--- TwoFishDH.py
+++ TwoFishDH.py
@@ -9,15 +9,32 @@ def menu(options_list):
 
 def encrypt():
     ipaddress = (input('Enter the ip address you want to encrypt'))
-    iplist = ipadress.split('.')
+    iplist = ipaddress.split('.')
     ip1 = iplist[0]
     ip2 = iplist[1]
     ip3 = iplist[2]
     ip4 = iplist[3]
+    #Convert to binary
+    ippart1 = int(ip1);
+    n=int(input('please enter the no. in decimal format: '))
+    x=n
+    k=[]
+    while (n>0):
+        a=int(float(n%2))
+        k.append(a)
+        n=(n-a)/2   
+    k.append(0)
+    string=""
+    for j in k[::-1]:
+        string=string+str(j)
+    print('The binary no. for %d is %s'%(x, string))
     #Xor stuff later
-
-
-
+    #
+    #a = "11011111101100110110011001011101000"
+    #b = "11001011101100111000011100001100001"
+    #y = int(a,2) ^ int(b,2)
+    #print '{0:b}'.format(y)
+    #Some xor stuff as reference
 
 
 
