0b7a594f84b27afed1eac9bad358b4740f8f4f3f
Ling Hui Gladys Chua <s10196678@connect.np.edu.sg>
2020-02-01 19:18:20 +0800

    vdfv

--- TwoFishDH.py
+++ TwoFishDH.py
@@ -9,7 +9,7 @@ def PHTe(dh,a,b):
     binv = ((a+(2*b)) % (2**dh))
     return ainv,binv  
 
-def PHTd(dh):
+def PHTd(dh,ainv,binv):
     a = (((2*ainv) - binv) % (2**dh))
     b = ((binv - ainv) % (2 **dh))
     return a,b
@@ -49,54 +49,7 @@ def myXOR(x, y):
         res |= xoredBit 
     return res
 
-xorkey0 = random.randint(0,255)
-xorkey1 = random.randint(0,255)
-xorkey2 = random.randint(0,255)
-xorkey3 = random.randint(0,255)
 
-#Split IP Address into 4 parts
-ipaddress = (input('Enter the ip address you want to encrypt: '))
-iplist = ipaddress.split('.')
-ip1 = int(iplist[0])
-ip2 = int(iplist[1])
-ip3 = int(iplist[2])
-ip4 = int(iplist[3])
-
-#XOR of xorkey and IP Address
-x0 = ip1 
-y0 = xorkey0
-fxor1 = myXOR(x0,y0)
-
-x1 = ip2
-y1 = xorkey1
-fxor2 = myXOR(x1,y1)
-
-x2 = ip3
-y2 = xorkey2
-fxor3 = myXOR(x2,y2)
-
-x3 = ip4
-y3 = xorkey3
-fxor4 = myXOR(x3,y3)
-print(fxor1 ,fxor2, fxor3, fxor4)
-
-dh = dh()
-print(dh)
-#PHT using DH as modulus and IP Address into per 2 bytes
-ainv , binv = PHTe(dh,fxor1,fxor2)
-ipa1 = ainv
-ipa2 = binv
-cinv , dinv = PHTe(dh,fxor3,fxor4)
-ipa3 = cinv
-ipa4 = dinv
-
-Sxor1 = myXOR(ipa1,xorkey0)
-Sxor2 = myXOR(ipa2,xorkey1)
-Sxor3 = myXOR(ipa3,xorkey2)
-Sxor4 = myXOR(ipa4,xorkey3)
-
-Encrypt = str(Sxor1) + '.' + str(Sxor2) + '.' + str(Sxor3) + '.' + str(Sxor4)
-print(Encrypt)
 
 def menu(options_list):
     print('Two Fish-DH\n=========')
@@ -107,15 +60,63 @@ def menu(options_list):
 
 
 
-#while True:
-#    menu(options_list)
-#    option = int(input("Enter your option: "))
-#    if (option == 0):
-#        break;
-#    elif option == 1:
-#        encrypt()
-#    else:
-#        decrypt()
+while True:
+    menu(options_list)
+    option = int(input("Enter your option: "))
+    if (option == 0):
+        break;
+    elif option == 1:
+        xorkey0 = random.randint(0,255)
+        xorkey1 = random.randint(0,255)
+        xorkey2 = random.randint(0,255)
+        xorkey3 = random.randint(0,255)
+
+        #Split IP Address into 4 parts
+        ipaddress = (input('Enter the ip address you want to encrypt: '))
+        iplist = ipaddress.split('.')
+        ip1 = int(iplist[0])
+        ip2 = int(iplist[1])
+        ip3 = int(iplist[2])
+        ip4 = int(iplist[3])
+
+        #XOR of xorkey and IP Address
+        x0 = ip1 
+        y0 = xorkey0
+        fxor1 = myXOR(x0,y0)
+
+        x1 = ip2
+        y1 = xorkey1
+        fxor2 = myXOR(x1,y1)
+
+        x2 = ip3
+        y2 = xorkey2
+        fxor3 = myXOR(x2,y2)
+
+        x3 = ip4
+        y3 = xorkey3
+        fxor4 = myXOR(x3,y3)
+        print(fxor1 ,fxor2, fxor3, fxor4)
+
+        dh = dh()
+        print(dh)
+        #PHT using DH as modulus and IP Address into per 2 bytes
+        ainv , binv = PHTe(dh,fxor1,fxor2)
+        ipa1 = ainv
+        ipa2 = binv
+        cinv , dinv = PHTe(dh,fxor3,fxor4)
+        ipa3 = cinv
+        ipa4 = dinv
+
+        Sxor1 = myXOR(ipa1,xorkey0)
+        Sxor2 = myXOR(ipa2,xorkey1)
+        Sxor3 = myXOR(ipa3,xorkey2)
+        Sxor4 = myXOR(ipa4,xorkey3)
+
+        Encrypt = str(Sxor1) + '.' + str(Sxor2) + '.' + str(Sxor3) + '.' + str(Sxor4)
+        print(Encrypt)
+       
+    else:
+       decrypt()
         
 
 
