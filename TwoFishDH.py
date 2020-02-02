1138496c0665e4e4524c281fb1ee8619f1945d3f
Ling Hui Gladys Chua <s10196678@connect.np.edu.sg>
2020-02-02 22:08:31 +0800

    werwe

--- TwoFishDH.py
+++ TwoFishDH.py
@@ -1,19 +1,23 @@
 #Done by Gladys Chua Ling Hui, Tan Yuan Ming and Chua Zhe Yu 
 import random
-#KprA = A = a^al mod p			KprB = B = a^b mod p
-#KpubA = B^al = (a^b)^al mod p		KpubB = A^b = (a^al)^b mod p
-#KAB = B^al = (a^b)^al mod p		KAB = A^b = (a^al)^b mod p
-
+#def functions PHT encrypt and decrypt
 def PHTe(a,b,dh):
-    ainv = ((a+b) % (2**dh))
-    binv = ((a+(2*b)) % (2**dh))
-    return ainv,binv  
-
-def PHTd(ainv,binv,dh):
-    a = (((2*ainv) - binv) % (2**dh))
-    b = ((binv - ainv) % (2 **dh))
+    
+    divided1 = ( a+b)//2**dh
+    divided2 = (a+ 2 *b)//2**dh
+    ainv = ( a+b) % 2**dh #remainder
+    binv =(a+ 2 *b)% 2**dh #remainder
+    
+    return divided1,divided2,ainv,binv
+    
+def PHTd(divided1,divided2,ainv,binv,oa,ob):
+    ab = (((2**dh) * divided1) + ainv)   #reverse to get original a+b
+    abb = ((2**dh)*divided2) + binv #reverse to get original a+2b
+    a = ab - ob
+    b = abb - ob - oa
     return a,b
 
+#def function for dh to implement into pht
 def dh():
     mod = 7
     base = 9
@@ -23,21 +27,14 @@ def dh():
     dh = (s1**b) % mod
     return dh
 
+#def function for displaymenu
 def menu():
     print('Two Fish-DH\n=========')
     print("1. Select Option 1 to Encrypt")
     print("2. Select Option 2 to Decrypt")
-    print('[0] Exit')
+    print('[0] Exit\n')
 
-#x = 10 (0000 1010), y = 4 (0000 0100),
-#Operator	Meaning	        Example
-#&	Bitwise AND	        x& y = 0 (0000 0000)
-#|	Bitwise OR	        x | y = 14 (0000 1110)
-#~	Bitwise NOT	        ~x = -11 (1111 0101)
-#^	Bitwise XOR 	    x ^ y = 14 (0000 1110)
-#>>	Bitwise right shift	x>> 2 = 2 (0000 0010)
-#<<	Bitwise left shift	x<< 2 = 40 (0010 1000)
-
+#def functoin for xor
 def myXOR(x, y): 
     xb = bin(x)
     yb = bin(y)
@@ -51,9 +48,10 @@ def myXOR(x, y):
             xor += '1'
         else:
             xor += '0'
-    print(int(xor,2))
+   
     return int(xor,2)
 
+#main 
 dh = dh()
 xorkey0 = random.randint(0,255)
 xk0 = xorkey0
@@ -64,6 +62,7 @@ xk2 = xorkey2
 xorkey3 = random.randint(0,255)
 xk3 = xorkey3
 
+#while loop for menu
 while True:
     menu()
     try:
@@ -84,25 +83,25 @@ while True:
         ip4 = int(iplist[3])
 
         ##XOR of xorkey and IP Address
-        #fxor1 = myXOR(ip1,xk0)
-        #fxor2 = myXOR(ip2,xk1)
-        #fxor3 = myXOR(ip3,xk2)
-        #fxor4 = myXOR(ip4,xk3)
-        #print(fxor1 ,fxor2, fxor3, fxor4)
+        fxor1 = myXOR(ip1,xk0)
+        fxor2 = myXOR(ip2,xk1)
+        fxor3 = myXOR(ip3,xk2)
+        fxor4 = myXOR(ip4,xk3)
+
 
         #PHT using DH as modulus and IP Address into per 2 bytes
         #ainv , binv = PHTe(fxor1,fxor2,dh)
         #cinv , dinv = PHTe(fxor3,fxor4,dh)
-        ainv , binv = PHTe(ip1,ip2,dh)
-        cinv , dinv = PHTe(ip1,ip2,dh)
-        
+        divided1,divided2,ainv , binv = PHTe(fxor1,fxor2,dh)
+        divided3,divided4,cinv , dinv = PHTe(fxor3,fxor4,dh)
+  
         #XOR again with the same key
         Sxor1 = myXOR(ainv,xk0)
         Sxor2 = myXOR(binv,xk1)
         Sxor3 = myXOR(cinv,xk2)
         Sxor4 = myXOR(dinv,xk3)
         Encrypt = str(Sxor1) + '.' + str(Sxor2) + '.' + str(Sxor3) + '.' + str(Sxor4)
-        print(Encrypt)
+        print("The Encrypted Ip Address is: ",Encrypt,"\n")
        
     elif option == 2:
         ipaddr = input('Enter the ip address you want to decrypt (must be immediate, do not close interface): ')
@@ -116,17 +115,17 @@ while True:
         Rxor2 = myXOR(dip2,xk1)
         Rxor3 = myXOR(dip3,xk2)
         Rxor4 = myXOR(dip4,xk3)
-        print(Rxor1 ,Rxor2, Rxor3, Rxor4)
+     
         #reverse pht
-        ra , rb = PHTd(Rxor1,Rxor2,dh)
-        rc , rd = PHTd(Rxor3,Rxor4,dh)
+        ra , rb = PHTd(divided1,divided2,Rxor1,Rxor2,fxor1,fxor2)
+        rc , rd = PHTd(divided3,divided4,Rxor3,Rxor4,fxor3,fxor4)
         #reverse xor
-        #opa1 = myXOR(ra,xk0)
-        #opa2 = myXOR(rb,xk1)
-        #opa3 = myXOR(rc,xk2)
-        #opa4 = myXOR(rd,xk3)
-        decrypt = str(ra) + "." + str(rb) +'.' + str(rc) + '.' +  str(rd)
-        print(decrypt)
+        opa1 = myXOR(ra,xk0)
+        opa2 = myXOR(rb,xk1)
+        opa3 = myXOR(rc,xk2)
+        opa4 = myXOR(rd,xk3)
+        decrypt = str(opa1) + "." + str(opa2) +'.' + str(opa3) + '.' +  str(opa4)
+        print("The decrypted Ip Address is: ",decrypt,"\n")
     else:
         print("Invalid Option")
         continue
